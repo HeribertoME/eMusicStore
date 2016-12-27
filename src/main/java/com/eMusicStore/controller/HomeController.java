3d0ddf2586edac2ba4557f8e6a0f790d6dbc95d2
Heriberto Martinez Elizarraraz <hmelizarraraz@hotmail.com>
2016-12-27 15:20:31 -0600

    Adding Product Model

--- src/main/java/com/eMusicStore/controller/HomeController.java
+++ src/main/java/com/eMusicStore/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.eMusicStore.controller;
 
+import com.eMusicStore.dao.ProductDao;
+import com.eMusicStore.model.Product;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by heriberto on 27/12/16.
@@ -10,8 +15,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+    private ProductDao productDao = new ProductDao();
+
     @RequestMapping("/")
     public String home() {
         return "home";
+    }
+
+    @RequestMapping("/productList")
+    public String getProducts(Model model) {
+        List<Product> productList = productDao.getProductList();
+        Product product = productList.get(0);
+        model.addAttribute(product);
+
+        return "productList";
     }
 }
