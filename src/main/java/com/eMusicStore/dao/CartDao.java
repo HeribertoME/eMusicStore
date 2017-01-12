b292865fca14308ecda78a28076e16846d823ab7
Heriberto Martinez Elizarraraz <hmelizarraraz@hotmail.com>
2017-01-11 18:11:33 -0600

    Configuring web flow

--- src/main/java/com/eMusicStore/dao/CartDao.java
+++ src/main/java/com/eMusicStore/dao/CartDao.java
@@ -2,12 +2,16 @@ package com.eMusicStore.dao;
 
 import com.eMusicStore.model.Cart;
 
+import java.io.IOException;
+
 /**
  * Created by heriberto on 10/01/17.
  */
 public interface CartDao {
 
     Cart getCartById(int cartId);
+
+    Cart validate(int cartId) throws IOException;
 
     void update(Cart cart);
 }
